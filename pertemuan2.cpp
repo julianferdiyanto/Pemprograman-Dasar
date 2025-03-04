f270898cc2e7c859b5cdfc6d331ad4b34416e638
julianferdiyanto <julianferdiyanto3@gmail.com>
2025-03-04 11:42:11 +0700

    introduction c++

---
+++ pertemuan2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    char jenisKelamin = 'L';
+    string jenis = "Laki-Laki";
+    float x = 10.5;
+    int y = 5;
+    bool check = true;
+
+    cout << "selamat datang di TI UMY\n";
+    cout << "Yogyakarta" << endl;
+    cout << "Indonesia";
+}
